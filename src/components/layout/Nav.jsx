d06041805604b21a7c6c448ebef8a80e7f97f125
Connor O'Brien <connorbobrien@gmail.com>
2023-12-13 01:23:57 -0300

    Fix cart link

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -6,18 +6,14 @@ export default function Nav() {
 		<nav className="bg-gray-800 text-white p-4">
 			<div className="container mx-auto flex justify-between items-center">
 				<div>
-					{/* <a href="/" className="flex items-center">
-            <HomeIcon className="h-6 w-6 mr-2"/>
-            Home
-          </a> */}
 					<NavLink to="/" className="flex items-center">
 						Home
 					</NavLink>
 				</div>
 				<div>
-					<a href="/cart" className="flex items-center">
+					<NavLink to="/cart" className="flex items-center">
 						<ShoppingCartIcon className="h-6 w-6 mr-2" />
-					</a>
+					</NavLink>
 				</div>
 			</div>
 		</nav>
